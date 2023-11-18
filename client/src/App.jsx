fd48b9bbc49eeb84ae78d6e5010892cb2c1f3fc0
Nước Mắm Code <nateng98@users.noreply.github.com>
2023-11-18 15:04:10 -0600

    Create front end

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Post from "./Post";
+import Header from "./Header";
+import { Routes, Route } from "react-router-dom"; // Learn more
+import Layout from "./Layout";
+
+// from pages folder
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />}/>
+        <Route path={'/login'} element={<LoginPage />}/>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/Post.jsx
@@ -0,0 +1,19 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="" alt="" />
+      </div>
+      <div className="texts">
+        <h2>Full-house</h2>
+        <p className="info">
+          <a href="" className="author">
+            Nathan
+          </a>
+          <time>2023-01-06 16:45</time>
+        </p>
+        <p className="summary">paragraph</p>
+      </div>
+    </div>
+  );
+}
