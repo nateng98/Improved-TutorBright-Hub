3db1b1a9fe3332102bd574a9bad0689589579c3f
Nước Mắm Code <nateng98@users.noreply.github.com>
2023-12-09 23:31:21 -0600

    create mongoose db and schema

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// connect to Mongoose Database
+mongoose.connect('mongodb+srv://nateng98:EqZnMVSootHXtxhG@pyroscript.neimdc1.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register', (req, res) => {
   const {username, password} = req.body;
