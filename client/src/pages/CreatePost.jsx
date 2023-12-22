0169fd1afcd7082f66d1c57219873d347ef1603f
Nước Mắm Code <nateng98@users.noreply.github.com>
2023-12-21 21:18:31 -0600

    add textarea for createpost page
    using react quill

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css'
+
 export default function CreatePost(){
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote', 'code block',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ];
   return(
     <form action="">
       <input type="title" placeholder={'Title'}/>
       <input type="summary" placeholder={'Summary'}/>
       <input type="file" />
-      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <ReactQuill value={content} formats={formats} />
+      <button style={{marginTop: '5px'}}>Creat post</button>
     </form>
   ); 
 }
