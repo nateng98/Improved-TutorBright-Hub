23f7f7a160b56dbe1330ca00968d888bc19251bc
Nước Mắm Code <nateng98@users.noreply.github.com>
2023-12-21 21:45:20 -0600

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,19 +6,40 @@ export default function CreatePost(){
   const [title, setTitle] = useState('');
   const [summary, setSummary] = useState('');
   const [content, setContent] = useState('');
+  
+  const modules = {
+    toolbar: [
+      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+      [{size: []}],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, 
+       {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image', 'video'],
+      ['clean']
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    }
+  };
 
   const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote', 'code block',
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image'
+    'link', 'image', 'video'
   ];
+
   return(
     <form action="">
       <input type="title" placeholder={'Title'}/>
       <input type="summary" placeholder={'Summary'}/>
       <input type="file" />
-      <ReactQuill value={content} formats={formats} />
+      <ReactQuill 
+        value={content} 
+        modules={modules}
+        formats={formats}
+      />
       <button style={{marginTop: '5px'}}>Creat post</button>
     </form>
   ); 
