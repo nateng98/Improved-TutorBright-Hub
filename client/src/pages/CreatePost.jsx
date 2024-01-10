b24a2365a322f51b9e6d01f627024ac34a3d958a
Nước Mắm Code <nateng98@users.noreply.github.com>
2024-01-10 17:22:17 -0600

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,63 +1,91 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css'
+import "react-quill/dist/quill.snow.css";
 
 const modules = {
   toolbar: [
-    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-    [{size: []}],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{'list': 'ordered'}, {'list': 'bullet'}, 
-     {'indent': '-1'}, {'indent': '+1'}],
-    ['link', 'image', 'video'],
-    ['clean']
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video"],
+    ["clean"],
   ],
   clipboard: {
     // toggle to add extra line breaks when pasting HTML:
     matchVisual: false,
-  }
+  },
 };
 
 const formats = [
-  'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image', 'video'
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
 ];
 
-export default function CreatePost(){
-  const [title, setTitle] = useState('');
-  const [summary, setSummary] = useState('');
-  const [content, setContent] = useState('');
-  const [files, setFiles] = useState('');
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
 
-  function createNewPost(ev){
+  function createNewPost(ev) {
     // create data object for 'a post'
     const data = new FormData();
-    data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
     // data.set('file', file);
 
     // ev.preventDefault();
     // fetch('http://localhost:4000/post', {
     //   method: 'POST',
-    //   body: 
+    //   body:
     // });
   }
 
-  return(
+  return (
     <form onSubmit={createNewPost}>
-      <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)}/>
-      <input type="summary" placeholder={'Summary'} value={summary} onChange={ev => setSummary(ev.target.value)}/>
-      <input type="file" value={files} onChange={ev => setFiles(ev.target.files)} />
-      <ReactQuill 
-        value={content} 
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type="file"
+        value={files}
+        onChange={(ev) => setFiles(ev.target.files)}
+      />
+      <ReactQuill
+        value={content}
         modules={modules}
         formats={formats}
-        onChange={newValue => setContent(newValue)}
+        onChange={(newValue) => setContent(newValue)}
       />
-      <button style={{marginTop: '5px'}}>Creat post</button>
+      <button style={{ marginTop: "5px" }}>Creat post</button>
     </form>
-  ); 
+  );
 }
